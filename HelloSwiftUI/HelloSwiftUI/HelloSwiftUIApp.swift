635af930940fb4a6a4a036ec82f6271e0cf4175d
Pengbo Xie <xpengbo@gmail.com>
2020-11-23 16:07:11 +0800

    Feature: Initial project

---
+++ HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by xpb on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
